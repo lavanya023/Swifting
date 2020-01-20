cac36859ec047c202c95a1fe80d078360a9cc235
Poomathi <Poomathi@LOGAMBIGAI-PC>
2020-01-20 16:28:57 +0530

    New Files Committed

---
+++ Swapping/src/news/alphanumeric.java
@@ -0,0 +1,12 @@
+package news;
+
+public class alphanumeric {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Swapping of two numbers");
+		// TODO Auto-generated method stub
+
+	}
+
+}
